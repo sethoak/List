7f4b3fc4f92ede227f12569253edc3775be69450
sethoak <sethoak@gmail.com>
2019-11-15 09:39:32 -0600

    saving files

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> cohorts = new List<int>();
+
+            cohorts.Add(34);
+            cohorts.Add(35);
+            cohorts.Add(36);
+            cohorts.Add(37);
+
+            //another way to initialize the list(as a string and not a INT)
+            List<string> classes = new List<string>()
+            {
+                "Cohort 34",
+                "Cohort 35",
+                "Cohort 36",
+                "Cohort 37"
+            };
+
+            classes.Remove("Cohort 34");
+            classes.Add("Cohort 38");
+
+        }
+    }
+}
